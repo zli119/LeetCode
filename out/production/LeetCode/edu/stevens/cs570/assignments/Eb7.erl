80e56a9fb6d28b2d89e3b5d4ac79bbbcf05d3249
zuoli <zli119@stevens.edu>
2020-03-28 17:31:00 -0400

    leetcode

---
+++ out/production/LeetCode/edu/stevens/cs570/assignments/Eb7.erl
@@ -0,0 +1,20 @@
+%%%-------------------------------------------------------------------
+%%% @author rocky
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%1. mult/2. Multiplies its two numeric arguments.
+%%2. double/1. Returns the double of the numeric argument.
+%%3. distance/2: consumes two tuples representing coordinates and returns the Euclidean distance between
+%%them.
+%%4. my_and/2. Use if.
+%%5. my_or/2. Use if.
+%%6. my_not/1. Use if.
+%%% @end
+%%% Created : 03. Jan 2020 4:27 PM
+%%%-------------------------------------------------------------------
+-module('Eb7').
+-author("rocky").
+
+%% API
+-export([]).
+
